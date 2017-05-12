27e9644ded8dbd5cdda51df414d499b16c0eae87
Chnkyfire <chnkyfire@Chnkyfires-Mac-mini.local>
2017-05-11 20:52:13 -0700

    Applied the mobile adjustments we made for episode 4 but not releasing since the episode has been preempted.  Added Audio to the transition video but made it so when user scrolls off the audio goes off.  Also changed the title names of some of the episodes.

--- architecture/js/custom.js
+++ architecture/js/custom.js
@@ -47,3 +47,19 @@ function closePlayer() {
 	//$('#apple-player').css("bottom","-506");
 }
 
+
+function isElementInViewport(elem) {
+    var $elem = $(elem);
+
+    // Get the scroll position of the page.
+    var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
+    var viewportTop = $(scrollElem).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+
+    // Get the position of the element on the page.
+    var elemTop = Math.round( $elem.offset().top );
+    var elemBottom = elemTop + $elem.height();
+
+    return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
+}
+
